c91b47fa8acb0e7d25562dd13b27e2ceabcc8a7d
Viktoriya-koy <vramonellrio@koyhq.com>
2025-09-14 21:18:27 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,6 @@
-// Configuración directa de Supabase
+// supabase.js - Versión para GitHub Pages
 const SUPABASE_URL = 'https://qpylzzjxbldtemrgqlja.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFweWx6emp4YmxkdGVtcmdxbGphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc4NTU4OTIsImV4cCI6MjA3MzQzMTg5Mn0.eZhPYRdS8Ptx2wS2Tt4QgRyP8ZGyTlObRe5V0VRQHiU';
+
+// Crear cliente de Supabase GLOBAL
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
